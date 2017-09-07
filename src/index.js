18c0ba6be1f1e54e3af25df9ca9c918eb85546bb
panforrest <guoqianp@gmail.com>
2017-09-07 05:55:46 -0400

    P1 9:30 change theme, revise src/index.js, run webpack -w

---
+++ src/index.js
@@ -0,0 +1,38 @@
+// import React, { Component } from 'react'
+// import ReactDOM from 'react-dom'
+// import { Elements } from './theme'
+
+//  The Elements components is a summary of basic presentation componets
+//  * available for use in this theme
+ 
+
+// class Example extends Component {
+//     render(){
+//         return (
+//             <div>
+//                 <Elements />
+//             </div>
+//         )
+//     }
+// }
+
+// ReactDOM.render(<Example />, document.getElementById('root'))
+
+
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { Elements } from './theme'
+import { Provider } from 'react-redux'
+import store from './stores'
+
+/* The Elements components is a summary of basic presentation componets
+ * available for use in this theme
+ */
+
+const app = (
+  <Provider store={store.configure(null)}>
+    <Elements />
+  </Provider>	
+)
+
+ReactDOM.render(app, document.getElementById('root'))
